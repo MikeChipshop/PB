2d69b4950cbe67230eab70b7d9da28b6388880dc
Mike Bishop <mike@miniman-webdesign.co.uk>
2016-10-20 10:43:57 +0100

    Layouts and styles for content pages
    Single and two column layouts and styling added for both desktop and
    mobile.

--- page.php
+++ page.php
@@ -4,8 +4,25 @@
         	<?php if ( have_posts() ) {?>
 					<?php while ( have_posts() ) : the_post(); ?>
                         <article id="pb_single-post-<?php the_ID(); ?>" <?php post_class('pb_content-page clearfix'); ?>>
-                        		<h3 class="pb_single-page-title"><?php the_title(); ?></h3>
-                            	<div class="pb_single-page-cont rte"><?php the_content(); ?></div>
+                        		<h3 class="pb_single-page-title"><?php the_title(); ?></h3>  
+                                <div class="pb_single-page-cont">
+                                	<?php if( have_rows('content_page_layouts') ): while ( have_rows('content_page_layouts') ) : the_row(); ?>
+                                    	<?php if( get_row_layout() == 'single_column_layout' ): ?>
+                                        	<div class="pb_one-col-layout rte">
+                                            	<?php the_sub_field('single_column_content'); ?>
+                                            </div>
+                                        <?php elseif( get_row_layout() == 'two_column_layout' ): ?>
+                                        	<div class="pb_two-col-layout">
+                                            	<div class="pb_two-col-one rte">
+                                                	<?php the_sub_field('two_column_content_one'); ?>
+                                                </div>
+                                                <div class="pb_two-col-two rte">
+                                                	<?php the_sub_field('two_column_content_two'); ?>
+                                                </div>
+                                            </div>
+                                        <?php endif; ?>
+                                    <?php endwhile; endif; ?>
+                                </div>
                     	</article>
                     <?php endwhile; ?>
             <?php } else { ?>
