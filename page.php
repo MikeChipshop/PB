2f83b7fbab0425c886eed047ceb7082f3e6975c7
Mike Bishop <mike@miniman-webdesign.co.uk>
2016-10-31 14:59:44 +0000

    Added conditional for WooC loop

--- page.php
+++ page.php
@@ -6,23 +6,27 @@
                         <article id="pb_single-post-<?php the_ID(); ?>" <?php post_class('pb_content-page clearfix'); ?>>
                         		<h3 class="pb_single-page-title"><?php the_title(); ?></h3>  
                                 <div class="pb_single-page-cont">
-                                	<?php if( have_rows('content_page_layouts') ): while ( have_rows('content_page_layouts') ) : the_row(); ?>
-                                    	<?php if( get_row_layout() == 'single_column_layout' ): ?>
-                                        	<div class="pb_one-col-layout rte">
-                                            	<?php the_sub_field('single_column_content'); ?>
-                                            </div>
-                                        <?php elseif( get_row_layout() == 'two_column_layout' ): ?>
-                                        	<div class="pb_two-col-layout">
-                                            	<div class="pb_two-col-one rte">
-                                                	<?php the_sub_field('two_column_content_one'); ?>
+                                	<?php if(is_account_page()): ?>
+                                    	<?php the_content(); ?>
+                                    <?php else: ?>
+										<?php if( have_rows('content_page_layouts') ): while ( have_rows('content_page_layouts') ) : the_row(); ?>
+                                            <?php if( get_row_layout() == 'single_column_layout' ): ?>
+                                                <div class="pb_one-col-layout rte">
+                                                    <?php the_sub_field('single_column_content'); ?>
                                                 </div>
-                                                <div class="pb_two-col-two rte">
-                                                	<?php the_sub_field('two_column_content_two'); ?>
+                                            <?php elseif( get_row_layout() == 'two_column_layout' ): ?>
+                                                <div class="pb_two-col-layout">
+                                                    <div class="pb_two-col-one rte">
+                                                        <?php the_sub_field('two_column_content_one'); ?>
+                                                    </div>
+                                                    <div class="pb_two-col-two rte">
+                                                        <?php the_sub_field('two_column_content_two'); ?>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        <?php endif; ?>
-                                    <?php endwhile; endif; ?>
-                                    <?php if(is_page('faqs')): ?>
+                                            <?php endif; ?>
+                                        <?php endwhile; endif; ?>
+                                    <?php endif; ?>
+									<?php if(is_page('faqs')): ?>
                                     	<?php if( have_rows('faqs') ): ?>
 											<div class="pb_faq-module">
                                             	<dl>
